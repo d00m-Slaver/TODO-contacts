73a7cff6262b63ab7e43898455914bde6cad3150
Anton Pinchuk Ivanovich <pinchukanton@list.ru>
2025-09-09 19:19:40 +0300

    feat(groups): обновлён файл с функциями для управления группами

--- src/scripts/groups-functions.ts
+++ src/scripts/groups-functions.ts
@@ -3,7 +3,23 @@ const groupsPanel = document.querySelector('.groups-panel') as HTMLDivElement;
 const overlay = document.querySelector('.overlay') as HTMLDivElement;
 const closeButton = document.querySelector('.groups-panel__close') as HTMLButtonElement;
 
+groupsBtn.addEventListener('click', openGroupPanel);
+overlay.addEventListener('click', closeGroupPanel);
+closeButton.addEventListener('click', closeGroupPanel);
+
+function updatePanelMode(){
+    if(window.innerWidth>=768){
+        groupsPanel.classList.add('desktop');
+        groupsPanel.classList.remove('mobile');
+    }   
+    else{
+        groupsPanel.classList.add('mobile');
+        groupsPanel.classList.remove('desktop');
+    }
+}
+
 function openGroupPanel() {
+    updatePanelMode();
     groupsPanel.classList.add('active');
     overlay.style.display = 'block'; 
 }
@@ -13,6 +29,9 @@ function closeGroupPanel() {
     overlay.style.display = 'none';
 }
 
-groupsBtn.addEventListener('click', openGroupPanel);
-overlay.addEventListener('click', closeGroupPanel);
-closeButton.addEventListener('click', closeGroupPanel);
+
+window.addEventListener('resize',()=>{
+    if(!groupsPanel.classList.contains('active')){
+        updatePanelMode();
+    }
+})
